acef53dabc56df26cb04230d89af5be4651e3730
Narendran <narendev0610@gmail.com>
2021-01-17 18:48:06 +0530

    usage of when to use type annotations and inference done

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -34,3 +34,36 @@ let point: { x: number; y: number } = {
 const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
+
+// ! When to use annotations
+
+//* 1) Function that returns the `any` type
+
+const json = '{"x":20,"y":42}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates);
+// Note: try to  Avoid `any` type at all cost
+
+// * 2) When we declare a variable on one line and initialize it later
+
+let words = ['red', 'green', 'blue'];
+// let foundWord;
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}
+
+// * 3) Variable whose type cannot be inferred correctly
+
+// {the following is not a ideal code example but to know it , it's used }
+let numbers = [-10, -232, 6];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; 1 < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
