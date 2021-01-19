debe610dd99dea56107886647ff578113786376b
Narendran <narendev0610@gmail.com>
2021-01-19 14:02:32 +0530

    nodemon & concurrently is used for execution automation, a simple bubble sort is implemented to sort out just the array of numbers,code to improve more

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,22 @@
-console.log('hello world!!');
+class Sorter {
+  constructor(public collection: number[]) {}
 
-const logSomething = (): void => {
-  console.log('Namasakram');
-};
+  sort(): void {
+    const { length } = this.collection;
 
-logSomething();
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const lefthand = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = lefthand;
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+
+console.log(sorter.collection);
