c4a16c0ed0b0765fa8e7a138740149281b881d87
Narendran <narendev0610@gmail.com>
2021-01-20 18:13:18 +0530

    refactored the app with the use of abstract class and explored the usefulness of abstract class

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,17 +1,14 @@
-import { Sorter } from './Sorter';
-// import { NumbersCollection } from './NumbersCollection';
-// import { CharactersCollection } from './CharactersCollection';
+import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
-// const numbersCollection = new NumbersCollection([-52, 52, 3, -5, 0]);
-// const sorter1 = new Sorter(numbersCollection);
-// sorter1.sort();
-// console.log(numbersCollection.data);
+const numbersCollection = new NumbersCollection([-52, 52, 3, -5, 0]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 
-// const charactersCollection = new CharactersCollection('Xanthyabryes');
-// const sorter2 = new Sorter(charactersCollection);
-// sorter2.sort();
-// console.log(charactersCollection.data);
+const charactersCollection = new CharactersCollection('Xanthyabryes');
+charactersCollection.sort();
+console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
 linkedList.add(6);
@@ -20,7 +17,6 @@ linkedList.add(37);
 linkedList.add(93);
 linkedList.add(-13);
 
-const sorter3 = new Sorter(linkedList);
-sorter3.sort();
+linkedList.sort();
 
 linkedList.print();
