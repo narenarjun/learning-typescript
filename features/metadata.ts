6d1ea3bbf0ad2fae43e02ca7008982b155dae940
Narendran <narendev0610@gmail.com>
2021-01-25 17:59:18 +0530

    started to refactor the app by using decorators & metadata, created a singleton of express router to use with the decorators, one route is modified and others are yet to be modified

--- features/metadata.ts
+++ features/metadata.ts
@@ -1,13 +1,40 @@
 import 'reflect-metadata';
 
-const plane = {
-  color: 'red',
-};
+@printMetadata
+class Plane {
+  color: string = 'red';
 
-Reflect.defineMetadata('note', 'hi there', plane, 'color');
+  @markfunction('Hello world')
+  fly(): void {
+    console.log('zzzhhhhhh');
+  }
+}
 
-const note = Reflect.getMetadata('note', plane, 'color');
-console.log(note);
+function markfunction(secretInfo: string) {
+  return function (target: Plane, key: string) {
+    Reflect.defineMetadata('secret', secretInfo, target, key);
+  };
+}
+
+function printMetadata(target: typeof Plane) {
+  for (let key in target.prototype) {
+    const secret = Reflect.getMetadata('secret', target.prototype, key);
+    console.log(secret);
+  }
+}
+
+// const secret = Reflect.getMetadata('secret', Plane.prototype, 'fly');
+
+// console.log(secret);
+
+// const plane = {
+//   color: 'red',
+// };
+
+// Reflect.defineMetadata('note', 'hi there', plane, 'color');
+
+// const note = Reflect.getMetadata('note', plane, 'color');
+// console.log(note);
 
 // Reflect.defineMetadata('note', 'hi there', plane);
 // Reflect.defineMetadata('height', '42', plane);
--- server/build/index.js
+++ server/build/index.js
@@ -7,10 +7,13 @@ var express_1 = __importDefault(require("express"));
 var loginRoutes_1 = require("./routes/loginRoutes");
 var body_parser_1 = __importDefault(require("body-parser"));
 var cookie_session_1 = __importDefault(require("cookie-session"));
+require("./controllers/LoginControllers");
+var Approuter_1 = require("./Approuter");
 var app = express_1.default();
 app.use(body_parser_1.default.urlencoded({ extended: true }));
 app.use(cookie_session_1.default({ keys: ['lkjhgfds'] }));
 app.use(loginRoutes_1.router);
+app.use(Approuter_1.AppRouter.getInstance());
 app.listen(3000, function () {
     console.log('Listening on port 3000');
 });
